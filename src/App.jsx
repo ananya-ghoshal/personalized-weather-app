3ae0771912686a61809905b76848a25bed3cd2ca
Ananya Ghoshal <ananyaghoshal.20@gmail.com>
2024-06-08 12:02:48 +0530

    dashboard buttons functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="container">
-      <Sidenav />
+    <>
       <Main />
-    </div>
+    </>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,6 +12,10 @@ import Snow from "../images/snow.png";
 import Wind from "../images/wind.png";
 import Pressure from "../images/pressure.png";
 import Sunny from "../images/sunny.jpg";
+import Dashboard from "../icons/dashboard.svg";
+import TrafficIcon from "../icons/traffic.svg";
+import CropIcon from "../icons/crops.svg";
+import EventIcon from "../icons/events.svg";
 
 function Main() {
   const [data, setData] = useState({});
@@ -26,84 +30,167 @@ function Main() {
       setLocation("");
     }
   };
-  let weather = document.getElementsByClassName("weather-card");
+
+  // // function to handle dashboard section
+  const handleDashboard = (event, tab) => {
+    let i, sidenavElement, userGroups;
+    // Get all elements with class="sidenavElement" and hide them
+    sidenavElement = document.getElementsByClassName("user-groups");
+    for (i = 0; i < sidenavElement.length; i++) {
+      sidenavElement[i].style.display = "none";
+    }
+
+    // Get all elements with class="userGroups" and remove the class "active"
+    userGroups = document.getElementsByClassName("user-groups");
+    for (i = 0; i < userGroups.length; i++) {
+      userGroups[i].className = userGroups[i].className.replace("active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tab).style.display = "flex";
+    event.currentTarget.className += " active";
+  };
   return (
-    <div className="main">
-      <div className="search">
-        <img src={Search} className="icons" />
-        <input
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
-          onKeyPress={searchLocation}
-          placeholder="Enter Location"
-          type="text"
-        />
+    <div className="container">
+      <div className="sidenav">
+        {/* dashboard button  */}
+        <button
+          className="sidenav-element"
+          onClick={(e) => handleDashboard(e, "dashboard")}
+        >
+          <img className="icons" src={Dashboard}></img>
+          Dashboard
+        </button>
+        {/* traffic button  */}
+        <button
+          className="sidenav-element"
+          onClick={(e) => handleDashboard(e, "traffic")}
+        >
+          <img className="icons" src={TrafficIcon}></img>
+          Traffic
+        </button>
+        {/* crops button */}
+        <button
+          className="sidenav-element"
+          onClick={(e) => handleDashboard(e, "crops")}
+        >
+          <img className="icons" src={CropIcon}></img>
+          Crop Yield
+        </button>
+        {/* events button  */}
+        <button
+          className="sidenav-element"
+          onClick={(e) => handleDashboard(e, "events")}
+        >
+          <img className="icons" src={EventIcon}></img>
+          Event Planning
+        </button>
       </div>
 
-      {data.name !== undefined && (
-        <>
-          <div className="hero">
-            <div className="weather-card">
-              <div className="location">
-                <p>{data.name}</p>
+      {/* Main section  */}
+
+      <div className="main">
+        <div className="search">
+          <img src={Search} className="icons" />
+          <input
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            onKeyPress={searchLocation}
+            placeholder="Enter Location"
+            type="text"
+          />
+        </div>
+
+        {data.name !== undefined && (
+          <>
+            <div className="hero">
+              <div className="weather-card">
+                <div className="location">
+                  <p>{data.name}</p>
+                </div>
+                <div className="weather-display">
+                  <div className="temp">
+                    {data.main.temp > 0 ? (
+                      <p className="tempDeg">
+                        {" "}
+                        + {data.main.temp.toFixed()} °C
+                      </p>
+                    ) : (
+                      <p className="tempDeg">
+                        {" "}
+                        - {data.main.temp.toFixed()} °C
+                      </p>
+                    )}
+                  </div>
+                  <div className="description">
+                    <p>{data.weather[0].main}</p>
+                  </div>
+                </div>
               </div>
-              <div className="weather-display">
-                <div className="temp">
+
+              {/* dashboard weather details according to user groups section  */}
+              <div
+                id="dashboard"
+                className="user-groups active"
+                style={{ display: "flex" }}
+              >
+                dashboard
+              </div>
+
+              <div id="traffic" className="user-groups">
+                traffic
+              </div>
+
+              <div id="crops" className="user-groups">
+                crops
+              </div>
+
+              <div id="events" className="user-groups">
+                events
+              </div>
+            </div>
+
+            <div className="weather-details">
+              <div className="details">
+                <p>Feels like</p>
+                <p style={{ color: "grey" }}>Today feels like</p>
+                <div className="weather-data">
+                  <img src={Clear} className="weather-icon" />
                   {data.main.temp > 0 ? (
-                    <p className="tempDeg"> + {data.main.temp.toFixed()} °C</p>
+                    <p> + {data.main.temp.toFixed()} °C</p>
                   ) : (
-                    <p className="tempDeg"> - {data.main.temp.toFixed()} °C</p>
+                    <p> - {data.main.temp.toFixed()} °C</p>
                   )}
                 </div>
-                <div className="description">
-                  <p>{data.weather[0].main}</p>
+              </div>
+              <div className="details">
+                <p>Humidity</p>
+                <p style={{ color: "grey" }}>Today Humidity </p>
+                <div className="weather-data">
+                  <img src={Humidity} className="weather-icon" />
+                  {data.main.humidity} %
+                </div>
+              </div>
+              <div className="details">
+                <p>Wind</p>
+                <p style={{ color: "grey" }}>Today wind speed </p>
+                <div className="weather-data">
+                  <img src={Wind} className="weather-icon" alt="" />
+                  {data.wind.speed} km/h
+                </div>
+              </div>
+              <div className="details">
+                <p>Pressure</p>
+                <p style={{ color: "grey" }}>Today air pressure</p>
+                <div className="weather-data">
+                  <img src={Pressure} className="weather-icon" />
+                  {data.main.pressure} hpa
                 </div>
               </div>
             </div>
-
-            <div className="map"></div>
-          </div>
-
-          <div className="weather-details">
-            <div className="details">
-              <p>Feels like</p>
-              <p style={{ color: "grey" }}>Today feels like</p>
-              <p className="weather-data">
-                <img src={Clear} className="weather-icon" />
-                {data.main.temp > 0 ? (
-                  <p> + {data.main.temp.toFixed()} °C</p>
-                ) : (
-                  <p> - {data.main.temp.toFixed()} °C</p>
-                )}
-              </p>
-            </div>
-            <div className="details">
-              <p>Humidity</p>
-              <p style={{ color: "grey" }}>Today Humidity </p>
-              <p className="weather-data">
-                <img src={Humidity} className="weather-icon" />
-                {data.main.humidity} %
-              </p>
-            </div>
-            <div className="details">
-              <p>Wind</p>
-              <p style={{ color: "grey" }}>Today wind speed </p>
-              <p className="weather-data">
-                <img src={Wind} className="weather-icon" alt="" />
-                {data.wind.speed} km/h
-              </p>
-            </div>
-            <div className="details">
-              <p>Pressure</p>
-              <p style={{ color: "grey" }}>Today air pressure</p>
-              <p className="weather-data">
-                <img src={Pressure} className="weather-icon" />
-                {data.main.pressure} hpa
-              </p>
-            </div>
-          </div>
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
